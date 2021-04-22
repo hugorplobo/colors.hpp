30635e49f11c01ae3dc780ede2aa46545998e7ea
Hugo <hugorplobo@gmail.com>
2021-04-22 14:58:15 -0300

    Add foreground and background named colors

--- include/colors.hpp
+++ include/colors.hpp
@@ -109,6 +109,326 @@ namespace colors {
         return stream;
     }
 
+    inline std::ostream& grey(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[30m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& red(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[31m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& green(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[32m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& yellow(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[33m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& blue(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[34m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& magenta(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[35m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& cyan(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[36m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& white(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[37m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& bright_grey(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[90m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& bright_red(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[91m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& bright_green(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[92m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& bright_yellow(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[93m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& bright_blue(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[94m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& bright_magenta(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[95m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& bright_cyan(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[96m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& bright_white(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[97m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_grey(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[40m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_red(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[41m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_green(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[42m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_yellow(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[43m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_blue(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[44m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_magenta(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[45m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_cyan(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[46m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_white(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[47m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_bright_grey(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[100m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_bright_red(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[101m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_bright_green(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[102m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_bright_yellow(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[103m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_bright_blue(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[104m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_bright_magenta(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[105m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_bright_cyan(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[106m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
+    inline std::ostream& on_bright_white(std::ostream& stream) {
+        #if defined(COLORS_USE_ANSI_ESCAPE)
+            stream << "\033[107m";
+        #else
+            stream << "Windows not supported yet";
+        #endif
+    
+        return stream;
+    }
+
 }
 
 #undef COLORS_TARGET_POSIX
